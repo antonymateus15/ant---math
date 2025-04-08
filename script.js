c3d05da34a293eef98c2d37db67acf6c9f81a05f
antonymateus15 <145035110+antonymateus15@users.noreply.github.com>
2025-04-08 11:02:20 -0300

    .

--- script.js
+++ script.js
@@ -16,3 +16,6 @@ document.addEventListener('DOMContentLoaded', function(){
     });
 
 });
+alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
+})
